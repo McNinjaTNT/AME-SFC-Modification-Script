15ea39b838df78608291a5378e58f26fa32a2e11
McNinjaTNT <69410086+McNinjaTNT@users.noreply.github.com>
2021-12-18 22:10:07 -0800

    Add files via upload

---
+++ AME-SFC-Modification-Script-v1.0.bat
@@ -0,0 +1,82 @@
+@ECHO OFF
+
+NET session > NUL 2>&1
+IF %ERRORLEVEL% LSS 1 (
+GOTO menu
+) ELSE (
+ECHO.
+ECHO  :: WINDOWS 10 AME SFC Modification Deployment Script
+ECHO.
+ECHO     You must run this script as Administrator
+ECHO.
+ECHO  :: Press any key to exit...
+PAUSE > NUL 2>&1
+GOTO EOF
+)
+
+:menu
+
+	ECHO.
+	ECHO  :: WINDOWS 10 AME Modification Deployment Script Version 2021.12.18
+	ECHO.
+	ECHO     This script deploys sfc.bat, renames sfc.exe, and modifies permissions for both
+	ECHO.
+	ECHO  :: Press any key to continue...
+	PAUSE > NUL
+	GOTO download
+
+:download
+
+	cls
+	ECHO  :: WINDOWS 10 AME Modification Deployment Script
+	ECHO.
+	ECHO.
+	ECHO.
+	ECHO  :: Downloading...
+
+	CURL -L https://github.com/McNinjaTNT/AME-SFC-Modification-Script/releases/download/v1.0/sfc.bat --output %SYSTEMROOT%\System32\sfc.bat > NUL 2>&1
+	IF %ERRORLEVEL% EQU 0 (
+		GOTO managePermissions
+	) ELSE (
+		
+		cls
+		ECHO  :: WINDOWS 10 AME Modification Deployment Script
+		ECHO.
+		ECHO.	 A failure occured while attempting to download the script.
+		ECHO.
+		ECHO  :: Press any key to exit...
+		PAUSE > NUL 2>&1
+		GOTO EOF
+	)
+
+:managePermissions
+
+	cls
+	ECHO  :: WINDOWS 10 AME Modification Deployment Script
+	ECHO.
+	ECHO.
+	ECHO.
+	ECHO  :: Changing Permissions and Renaming sfc.exe...
+
+	:: Copies the ACL from diskmgmt.msc to sfc.bat.
+	POWERSHELL -command "Get-Acl %SYSTEMROOT%\System32\diskmgmt.msc | Set-Acl %SYSTEMROOT%\System32\sfc.bat" > NUL 2>&1
+
+	:: Gives the Administrator group full access for renaming sfc.exe to sfc1.exe.
+	TAKEOWN /f %SYSTEMROOT%\System32\sfc.exe /a > NUL 2>&1
+	ICACLS %SYSTEMROOT%\System32\sfc.exe /grant Administrators:F > NUL 2>&1
+	GOTO renamesfc.exe
+
+:renamesfc.exe
+
+	REN %SYSTEMROOT%\System32\sfc.exe sfc1.exe > NUL 2>&1
+	:: Copies the ACL from diskmgmt.msc to sfc1.exe. Essentially resetting its ACL.
+	POWERSHELL -command "Get-Acl c:\Windows\System32\diskmgmt.msc | Set-Acl c:\Windows\System32\sfc1.exe" > NUL 2>&1
+
+	cls
+	ECHO  :: WINDOWS 10 AME Modification Deployment Script
+	ECHO.
+	ECHO.	 Deployment is complete!
+	ECHO.
+	ECHO  :: Press any key to exit...
+	PAUSE > NUL 2>&1
+	GOTO EOF
---
+++ sfc.bat
@@ -0,0 +1,167 @@
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+:: This script is to prevent users from unknowingly entering sfc /scannow
+:: and causing de-amelioration
+
+CMD /c "exit /b 0"
+NET session > NUL 2>&1
+IF %ERRORLEVEL% GTR 0 (
+
+	ECHO You must be an administrator running a console session in order to
+	ECHO use the sfc utility.
+		
+	GOTO :EOF
+) ELSE (
+	GOTO checkScannow
+)
+
+:checkScannow
+	
+	CMD /c "exit /b 0"
+	SET checkString=/scannow
+	ECHO %* | FIND /i %checkString% > NUL 2>&1
+	IF %ERRORLEVEL% EQU 0 (
+		
+		ECHO.
+		ECHO This command will cause de-amelioration^^! DO NOT RUN
+		ECHO Are you sure you want to run this command?
+		ECHO.
+		ECHO Enter "Cancel" to Exit
+		
+		SET /p input=Enter "I know what I'm doing" to Confirm: 
+
+			IF "!input!"=="I know what I'm doing" GOTO scannowProcedure+SelfDestruction
+			IF "!input!"=="Cancel" GOTO end
+		
+			ECHO.
+			ECHO Incorrect Input Entered.
+			
+			GOTO :EOF
+
+	) ELSE (
+		GOTO verifyOnlyProcedure
+	)
+
+:verifyOnlyProcedure
+	
+	CMD /c "exit /b 0"
+	SET checkString=/verifyonly
+	IF /i %*==%checkString% (
+
+		ECHO.
+		ECHO Beginning system scan.  This process will take some time.
+		TIMEOUT /T 1 /NOBREAK > NUL 2>&1
+		ECHO.
+		ECHO Beginning verification phase of system scan.
+		TIMEOUT /T 2 /NOBREAK > NUL 2>&1
+		ECHO Verifying...
+		
+		:: %* is all the text entered after "sfc ".
+		sfc1 %*
+
+		ECHO Windows Resource Protection found integrity violations.
+		ECHO For online repairs, details are included in the CBS log file located at
+		ECHO windir^\Logs^\CBS^\CBS.log. For example C^:^\Windows^\Logs^\CBS^\CBS.log. For offline
+		ECHO repairs, details are included in the log file provided by the ^/OFFLOGFILE flag.
+
+		GOTO :EOF
+	) ELSE (
+		GOTO incorrectSyntaxEmulation
+	)
+
+:incorrectSyntaxMessage
+	
+	CMD /c "exit /b 0"
+	sfc1 %*
+	IF %ERRORLEVEL% GTR 0 (
+
+	ECHO Microsoft ^(R^) Windows ^(R^) Resource Checker Version 6.0
+	ECHO Copyright ^(C^) Microsoft Corporation. All rights reserved.
+	ECHO.
+	ECHO Scans the integrity of all protected system files and replaces incorrect versions with
+	ECHO correct Microsoft versions.
+	ECHO.
+	ECHO SFC ^[^/SCANNOW^] ^[^/VERIFYONLY^] ^[^/SCANFILE^=^<file^>^] ^[^/VERIFYFILE^=^<file^>]
+	ECHO     ^[^/OFFWINDIR^=^<offline windows directory^> ^/OFFBOOTDIR^=^<offline boot directory^> ^[^/OFFLOGFILE^=^<log file path^>^]^]
+	ECHO. 
+	ECHO ^/SCANNOW        Scans integrity of all protected system files and repairs files with
+	ECHO                 problems when possible.
+	ECHO ^/VERIFYONLY     Scans integrity of all protected system files. No repair operation is
+	ECHO                 performed.
+	ECHO ^/SCANFILE       Scans integrity of the referenced file, repairs file if problems are
+	ECHO                 identified. Specify full path ^<file^>
+	ECHO ^/VERIFYFILE     Verifies the integrity of the file with full path ^<file^>.  No repair
+	ECHO                 operation is performed.
+	ECHO ^/OFFBOOTDIR     For offline repair, specify the location of the offline boot directory
+	ECHO ^/OFFWINDIR      For offline repair, specify the location of the offline windows directory
+	ECHO ^/OFFLOGFILE     For offline repair, optionally enable logging by specifying a log file path
+	ECHO. 
+	ECHO e.g.
+	ECHO. 
+	ECHO         sfc ^/SCANNOW
+	ECHO         sfc ^/VERIFYFILE^=c^:^\windows^\system32^\kernel32.dll
+	ECHO         sfc ^/SCANFILE^=d^:^\windows^\system32^\kernel32.dll ^/OFFBOOTDIR^=d^:^\ ^/OFFWINDIR^=d^:^\windows
+	ECHO         sfc ^/SCANFILE^=d^:^\windows^\system32^\kernel32.dll ^/OFFBOOTDIR^=d^:^\ ^/OFFWINDIR^=d^:^\windows ^/OFFLOGFILE^=c^:^\log.txt
+	ECHO         sfc ^/VERIFYONLY
+
+	GOTO :EOF
+	) ELSE (
+	GOTO grabCBSInfo
+	)
+
+:grabCBSInfo
+	
+	CMD /c "exit /b 0"
+	SET checkString="Could not reproject"
+	SET count=1
+	FOR /F "tokens=2 delims=]" %%a IN ('powershell -command "Get-Content '%SYSTEMROOT%\Logs\CBS\CBS.log' -tail 3"') DO (
+
+		SET var!count!=%%a
+		SET /a count=!count!+1
+		GOTO noViolationProcedure
+	)
+	
+
+:noViolationProcedure
+
+	SET checkString=Beginning
+	ECHO %var2% | FIND /i "%checkString%" > NUL 2>&1
+	IF %ERRORLEVEL% EQU 0 (
+	
+		ECHO Windows Resource Protection did not find any integrity violations.
+
+		GOTO :EOF
+	) ELSE (
+		GOTO foundViolationProcedure
+	)
+
+:foundViolationProcedure
+	CMD /c "exit /b 0"
+	SET checkString=reproject
+	ECHO %var1% | FIND /i "%checkString%" > NUL 2>&1
+	IF %ERRORLEVEL% EQU 0 (
+
+		ECHO Windows Resource Protection found integrity violations.
+		ECHO For online repairs, details are included in the CBS log file located at
+        ECHO windir^\Logs^\CBS\CBS.log. For example C^:^\Windows^\Logs^\CBS^\CBS.log. For offline
+		ECHO repairs, details are included in the log file provided by the ^/OFFLOGFILE flag.
+
+		GOTO :EOF
+		) ELSE (
+		GOTO :EOF
+		)
+
+:scannowProcedure+SelfDestruction
+:: This will cause sfc.bat to no longer function, unless sfc.bat is specified.
+:: This is due to the .exe extension being prioritized over .bat. The PATHEXT environment variable can change this.
+TAKEOWN /f %SYSTEMROOT%\System32\sfc1.exe /a > NUL 2>&1
+ICACLS %SYSTEMROOT%\System32\sfc1.exe /grant Administrators:F > NUL 2>&1
+REN %SYSTEMROOT%\System32\sfc1.exe sfc.exe > NUL 2>&1
+:: Copy ACL from diskmgmt.msc to sfc.exe. Essentially resetting sfc.exe's ACL.
+POWERSHELL -command "Get-Acl %SYSTEMROOT%\System32\diskmgmt.msc | Set-Acl %SYSTEMROOT%\System32\sfc.exe" > NUL 2>&1
+sfc %*
+:: Self-destruction
+TAKEOWN /f %SYSTEMROOT%\System32\sfc.bat /a > NUL 2>&1
+ICACLS %SYSTEMROOT%\System32\sfc.bat /grant Administrators:F > NUL 2>&1
+(GOTO) 2>NUL & del "%~f0" > NUL 2>&1
